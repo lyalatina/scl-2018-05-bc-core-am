9c5372e6a604727073379b9d136c3ace15cd544b
Natalia <nataliamp87@gmail.com>
2018-05-27 17:18:40 -0400

    orden ficheros

---
+++ js/index2.js
@@ -0,0 +1,34 @@
+window.onload =() => {
+  
+  //Ingresar palabra clave
+  
+      const enteredName = document.getElementById("enteredName")
+      enteredName.addEventListener("click", function () {
+  
+        
+      username = document.getElementById("username").value;
+      document.getElementById("getUsername").className = "hidden";
+      document.getElementById("demo").innerHTML = "Correcto " + username;
+  
+  
+  });
+  
+  var messages = ['Puedes ingresar'];
+    
+   var score = 0;
+  
+    
+    if(username == "Claudio" && "Marcela" && "Helena" && "Alex") {
+  
+  
+    }
+    
+    
+    //
+  
+  
+  
+    
+  
+  }
+  
