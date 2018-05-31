67308eda6ea3c97da09a76640e373dc91f1ec0d3
Natalia <nataliamp87@gmail.com>
2018-05-30 23:36:19 -0400

    encode funciona

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,17 @@
 window.cipher = {
-
-};
   //Empieza el cifrado
   encode: (text, key) => {
- 
+  let result = '';
+  for (let i = 0; i < text.length; i++) {
+  let outputAscii = (text.charCodeAt(i) - 65 + parseInt(key)) % 26 + 65; //Asscii
 
+  if (text.charCodeAt(i) === 32 ) {
+    outputAscii = " ";
+    result += ""
   }
+  result += String.fromCharCode(outputAscii)
+}  
+  return result;
+},
+//Termina el cifrado
+}
